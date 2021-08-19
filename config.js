1fd7d828b735e06aef8eb404d7342fb7a57a0d2d
baogia0912 <51979397+baogia0912@users.noreply.github.com>
2021-08-19 23:28:37 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Em à',
+    introDesc: `Anh có điều này muốn hỏi em, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'oke',
+    title: 'Em yêu anh có phải không 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là em là vợ anh đó nha :> ',
+    btnYes: 'yêu ơi là iuuu <33',
+    btnNo: 'Em mơ à :333',
+    question:'Nói cho anh lý do vì sao em yêu anh đii <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Tại vì Em đẹp trai hihi <33333333',
+    mess: 'Anh biết mà 🥰. Love you 300.000 😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho anh đi nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/giabao.ha.988'
+}
